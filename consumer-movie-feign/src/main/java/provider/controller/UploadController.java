d51fcdbd74cdc2a8f255b023668b508fae6b95fd
JulyJunWu <596889735@qq.com>
2019-10-16 23:15:15 +0800

    spring Cloud微服务 zuul上传文件

---
+++ consumer-movie-feign/src/main/java/provider/controller/UploadController.java
@@ -0,0 +1,35 @@
+package provider.controller;
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Jun
+ * data  2019-10-16 22:46
+ */
+
+@RestController
+public class UploadController {
+
+    /**
+     * 上传文件
+     *  在linux上使用 curl -F "file=@文件名" http://localhost:8082/upload
+     */
+    @PostMapping("/upload")
+    public String upload(@RequestParam(value = "file",required = true) MultipartFile file)throws IOException{
+
+        byte[] bytes = file.getBytes();
+        String originalFilename = file.getOriginalFilename();
+        File fileToSave = new File(originalFilename);
+
+        FileCopyUtils.copy(bytes,fileToSave);
+        return fileToSave.getAbsolutePath();
+    }
+
+}
